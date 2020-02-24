545762699f86db6691f6419e08492d6803d66380
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-24 09:46:46 +0300

    add profile edit method to edit the user profile

--- hood/views.py
+++ hood/views.py
@@ -55,3 +55,17 @@ def profile(request):
     profile = Profile.objects.get(user=current_user.id)
     hoods = Neighbourhood.objects.all()
     return render(request, 'profile/profile.html', {"date": date, "profile":profile,"hoods":hoods})
+
+def edit_profile(request):
+    date = dt.date.today()
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user.id)
+    if request.method == 'POST':
+        signup_form = EditForm(request.POST, request.FILES,instance=request.user.profile)
+        if signup_form.is_valid():
+            signup_form.save()
+            return redirect('profile')
+    else:
+        signup_form =EditForm()
+
+    return render(request, 'profile/edit_profile.html', {"date": date, "form":signup_form,"profile":profile})
