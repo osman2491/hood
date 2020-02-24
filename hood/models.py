dfb229b66ff2ec6da543c85edf9fb6e6a85ce0e3
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-24 09:39:00 +0300

    added location and category  model

--- hood/models.py
+++ hood/models.py
@@ -69,3 +69,27 @@ class Post(models.Model):
     user = models.ForeignKey(Profile, related_name='profile')
     post = models.CharField(max_length=30)
     neighbourhood = models.ForeignKey(Neighbourhood, related_name='posts')
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
