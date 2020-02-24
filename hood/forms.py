197456e80081ed1882a0f60ec6d12c1548a62c1f
osman67239121@gmail.com <osman67239121@gmail.com>
2020-02-24 09:57:36 +0300

    add edit form for profile

--- hood/forms.py
+++ hood/forms.py
@@ -8,3 +8,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_pic']
